5645cb2238ac1ddcac9f3fb0e61f03fcbdb5828d
Yoodahun <dahun4032@gmail.com>
2020-12-14 23:33:06 +0900

    ADD Marker skip and expected Fail. also reason

--- tests/tests/test_environment.py
+++ tests/tests/test_environment.py
@@ -2,6 +2,7 @@ from pytest import mark
 
 
 @mark.env
+@mark.skip(reason="broken QA env by deploy somenumber")
 def test_environment_is_qa(app_config):
     assert app_config.base_url == 'https://myqa-env.com'
 
@@ -9,3 +10,15 @@ def test_environment_is_qa(app_config):
 @mark.env
 def test_environment_is_dev(app_config):
     assert app_config.base_url == 'https://mydev-env.com'
+
+
+@mark.env
+@mark.skip(reason="broken Staging env by deploy somenumber")
+def test_environment_is_staging(app_config):
+    assert app_config.base_url == 'https://mystaging-env.com'
+
+@mark.env
+@mark.xfail(reason="Not ready alpha env yet")
+def test_environment_is_alpha(app_config):
+    assert app_config.base_url == 'https://myalpha-env.com'
+    assert True
